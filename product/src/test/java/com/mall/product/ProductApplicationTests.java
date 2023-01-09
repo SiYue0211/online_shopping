f39e1ffc75d876c82ae01f92969b5b23a5a9a96d
yue <siyue0112@126.com>
2023-01-09 22:40:54 +0000

    add CRUD in product

--- product/src/test/java/com/mall/product/ProductApplicationTests.java
+++ product/src/test/java/com/mall/product/ProductApplicationTests.java
@@ -1,13 +1,40 @@
 package com.mall.product;
 
-import org.junit.jupiter.api.Test;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mall.product.entity.BrandEntity;
+import com.mall.product.service.BrandService;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ProductApplicationTests {
+public class ProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("Apple Pro");
+//        brandService.updateById(brandEntity);
+//        System.out.println("Update successfully");
+
+//        brandEntity.setName("Apple");
+//        brandService.save(brandEntity);
+//        System.out.println("Save successfully!");
+
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach((item) -> {
+            System.out.println(item);
+        });
     }
 
 }
